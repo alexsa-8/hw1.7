15d49eb75f62a7eff08f64c3adb9a97ab98aa272
Alex <alexsa-8@mail.ru>
2022-08-17 23:11:47 +0300

    3 задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("\nЗадание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        //Задание 2
+        System.out.println("\nЗадание 2");
+        String bigFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + bigFullName);
+        //Задание 3
+        System.out.println("\nЗадание 3");
+        StringBuilder sb = new StringBuilder(fullName);
+        sb.delete(0, fullName.length());
+        sb.insert(0, "Иванов Семён Сумёнович");
+        String newFullName = sb.toString();
+        newFullName = newFullName.replace("ё","е");
+        System.out.println(newFullName);
     }
 }
