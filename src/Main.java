bedd6ba7844785025fefab9fd995b9a3a9fa2d8e
Alex <alexsa-8@mail.ru>
2022-08-19 22:38:02 +0300

    сделал 5 задание

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+//import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Задание 1
@@ -31,5 +32,38 @@ public class Main {
         String firstName1 = fullName.substring(index1, index2);
         String middleName1 = fullName.substring(index2, index3);
         System.out.println(lastName1 + "\n" + firstName1 + "\n" + middleName1);
+        //Задание 5
+        System.out.println("\nЗадание 5");
+        String lFullName = "ivanov ivan ivanovich";
+        System.out.println(lFullName);
+        String lFullN = lFullName.trim();
+        int i4 = lFullN.indexOf(' ');
+        int i5 = lFullN.indexOf(' ', i4 + 1);
+        int i8 = lFullN.length();
+        System.out.println("В строке " + lFullN + " пробел ' ' находится под индексом " + i4 + " " + i5 + ".");
+        System.out.println("Длина строки " + i8 + " ячейка.");
+        int iLast=0;
+        int iFirst= (i4+1);
+        int iMiddle= (i5+1);
+        System.out.println("Начальные буквы ФИО находятся под индексами "+ iLast+" "+ iFirst+" "+ iMiddle+".");
+        System.out.println("Верное написание Ф. И. О. сотрудника с заглавных букв — "
+                +lFullN.substring(0, 1).toUpperCase()+lFullN.substring(2,iFirst).toLowerCase()+ " "
+                +lFullN.substring(iFirst, iFirst+1).toUpperCase()+lFullN.substring(iFirst+1,iMiddle).toLowerCase()+ " "
+                +lFullN.substring(iMiddle, iMiddle+1).toUpperCase()+lFullN.substring(iMiddle+1,i8).toLowerCase());
+
+        //ещё один вариант решения Задачи 5 я нашёл в интернете. Разабраться в нём трудновато.
+        /*String upper_case_line = "";
+        String lFullName1 = "ivanov ivan ivanovich";
+        Scanner lineScan = new Scanner(lFullName1);
+        while(lineScan.hasNext()) {
+            String word = lineScan.next();
+            upper_case_line += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+        }
+        System.out.println(upper_case_line);*/
+        //Задание 6
+        System.out.println("\nЗадание 6");
+
+
+
     }
 }
